0f3b2ebf4d6d20b26d6c45a59910c3588e12eccc
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2025-04-30 15:16:19 +0530

    Datatypes in TS

---
+++ app.ts
@@ -0,0 +1,7 @@
+//Reference and Primitive Types
+// TypeScript is a superset of JavaScript that adds static typing
+//Reference types are [], (). {} the change it parellel to Parent and child
+//Primitive types are string, number, boolean, null, undefined, symbol, bigint
+// let a = [] 
+// let b = 12;    
+
---
+++ dtypes.ts
@@ -0,0 +1,13 @@
+// datatypes in Typescript 
+// Primitive Datatypes (Number , String, Boolean)
+//Arays
+// Tuples
+// Enums
+// Any unknown Void Null undefined
+
+let a = 12;
+let b = "malav"
+
+// Arrays
+let c = [1,2,3,4,5, "Malav", true]; // mixed array
+
