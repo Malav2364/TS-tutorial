dbd1e4f76c5ec044425a4a39a83fcb79ec9eee5c
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2025-04-30 15:35:58 +0530

    Dtypes complete basics

--- dtypes.js
+++ dtypes.js
@@ -9,3 +9,23 @@ let a = 12;
 let b = "malav";
 // Arrays
 let c = [1, 2, 3, 4, 5, "Malav", true]; // mixed array
+let arr1 = [1, 2, 3, 4, { name: "Malav" }];
+let arr2 = [1, 2, 3]; //Only Numbers are alloweed in this array number array
+//Tuples 
+let tpl1 = [1, "Malav"]; //tuples are having only 2 possible datatypes 
+//enums just like an object but it is a special type of object that allows you to define a set of named constants.
+//Enums are a way to give more friendly names to sets of numeric values.    
+var color;
+(function (color) {
+    color[color["red"] = 1] = "red";
+    color[color["green"] = 2] = "green";
+    color[color["blue"] = 3] = "blue";
+})(color || (color = {}));
+color.red;
+//Any Datatype
+//this is explicitly telling typescript that this variable can be of any type.
+let m;
+m = 10;
+//this will accept any type of value in it as no datatype is defined
+let n;
+n = 10;
--- dtypes.ts
+++ dtypes.ts
@@ -10,4 +10,29 @@ let b = "malav"
 
 // Arrays
 let c = [1,2,3,4,5, "Malav", true]; // mixed array
+let arr1 = [1,2,3,4,{name : "Malav"}]
 
+let arr2: number[] = [1,2,3]; //Only Numbers are alloweed in this array number array
+
+//Tuples 
+let tpl1: [number, string] = [1, "Malav"] //tuples are having only 2 possible datatypes 
+
+//enums just like an object but it is a special type of object that allows you to define a set of named constants.
+//Enums are a way to give more friendly names to sets of numeric values.    
+ enum color {
+    red = 1, 
+    green = 2, 
+    blue = 3
+ }
+
+ color.red
+
+
+ //Any Datatype
+ //this is explicitly telling typescript that this variable can be of any type.
+ let m: number;
+ m=10;
+
+//this will accept any type of value in it as no datatype is defined
+ let n;
+ n=10
