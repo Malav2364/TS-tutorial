4dc5f902edcf1b94fa3b1949e57a0dd9b91d4243
malav2364 <malavp024@gmail.com>
2025-05-13 22:57:13 +0530

    Changed OS

--- dtypes.js
+++ dtypes.js
@@ -1,17 +1,18 @@
+"use strict";
 // datatypes in Typescript 
 // Primitive Datatypes (Number , String, Boolean)
 //Arays
 // Tuples
 // Enums
 // Any unknown Void Null undefined
-var a = 12;
-var b = "malav";
+let a = 12;
+let b = "malav";
 // Arrays
-var c = [1, 2, 3, 4, 5, "Malav", true]; // mixed array
-var arr1 = [1, 2, 3, 4, { name: "Malav" }];
-var arr2 = [1, 2, 3]; //Only Numbers are alloweed in this array number array
+let c = [1, 2, 3, 4, 5, "Malav", true]; // mixed array
+let arr1 = [1, 2, 3, 4, { name: "Malav" }];
+let arr2 = [1, 2, 3]; //Only Numbers are alloweed in this array number array
 //Tuples 
-var tpl1 = [1, "Malav"]; //tuples are having only 2 possible datatypes 
+let tpl1 = [1, "Malav"]; //tuples are having only 2 possible datatypes 
 //enums just like an object but it is a special type of object that allows you to define a set of named constants.
 //Enums are a way to give more friendly names to sets of numeric values.    
 var color;
@@ -23,20 +24,20 @@ var color;
 color.red;
 //Any Datatype
 //this is explicitly telling typescript that this variable can be of any type.
-var m;
+let m;
 m = 10;
 //this will accept any type of value in it as no datatype is defined
-var n;
+let n;
 n = 10;
-var z;
+let z;
 z = 15;
-var x;
+let x;
 x = "Malav Patel";
 //Any in typescript means typescript off
-var j;
+let j;
 j = "Malav";
 //Unknown means we have to define for what type the value is 
-var d;
+let d;
 d = 10;
 d = "Malav Patel";
 //we have to define the type before using it 
@@ -44,11 +45,11 @@ d = "Malav Patel";
 if (d === "string") {
     d.toLowerCase();
 }
-var k = null; //here it is type any 
+let k = null; //here it is type any 
 k = 10;
-var l;
+let l;
 // l = "Malav"; //type null string not assignable
-var i;
+let i;
 // i = 10; //undefined is not assignable to number
 function nevr() {
     while (true) {
---
+++ typeInference.js
@@ -0,0 +1,20 @@
+"use strict";
+// Type Inference
+//Means the  given value is  inferred by the compiler
+//eg if a = 10 then its number  and if a = "10" then its string
+let a1 = "12"; // String
+let a2 = 12; // Number
+//Type annotations
+//So here we annotated that the value 
+let a3;
+//Or we can give any one of them like 
+let a4;
+// now we can assogn any type of assigned values
+a4 = true;
+a4 = 10;
+a4 = "true";
+//all accepted
+//also need to be done for parameters of a function
+function func1(a, b) {
+    return a + b;
+}
